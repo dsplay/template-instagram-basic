46d41746cc7f3200cb6ed8d564ec064c4485f0dc
Paulo Coutinho <pcmnac@gmail.com>
2020-06-21 19:05:04 +0100

    :recycle: consider falsy values

--- src/components/user-profile.jsx
+++ src/components/user-profile.jsx
@@ -15,21 +15,25 @@ const defaultPic = tval('profile_picture');
 const defaultUserScreenName = tval('user_screen_name');
 
 function UserProfile({
-    name = defaultUserScreenName,
+    name,
     username,
-    pic = defaultPic,
+    pic,
     className,
 }) {
+
+    const finalName = name || defaultUserScreenName;
+    const finalPic = pic || defaultPic;
+
     return (
         <div className={`user-profile ${className}`}>
-            <div className="user-picture" style={{ backgroundImage: `url("${pic}")` }}>
+            <div className="user-picture" style={{ backgroundImage: `url("${finalPic}")` }}>
                 {
                     showLogo && !showInfo &&
                     <img className="logo" alt="logo" src={logo}/>
                 }
             </div>
             <div className="user-info">
-                <span className="user-name" style={{ color: fullNameColor }}>{name}</span>
+                <span className="user-name" style={{ color: fullNameColor }}>{finalName}</span>
                 <span className="user-screen-name" style={{ color: screenNameColor }}>@{username}</span>
             </div>
         </div>
